5380a77834ab26fd6d66294827ac740ef8009b38
v_cyhk <v.iutucuc@gmail.com>
2022-04-25 14:41:10 +0800

    Initial commit

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -79,14 +79,44 @@ export default function MyAssets() {
                         {nft.name}
                   </p>
                   <p className="mb-3 text-2xl font-bold">Price - {nft.price} ETH</p>
-                  <button onClick={() => setSaleDetail(i)} 
-                  className="w-full bg-blue-600 font-bold py-2 px-12 rounded"
-                  data-bs-toggle="offcanvas" 
-                  data-bs-target="#offcanvasBottom" 
-                  aria-controls="offcanvasBottom"
-                  >
+                  <button type="button" class="w-full bg-blue-600 font-bold py-2 px-12 rounded" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#Modal"
+                        onClick={() => setSaleDetail(i)}>
                     Detail
                   </button>
+
+                  <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+                    id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+                    <div class="modal-dialog relative w-auto pointer-events-none">
+                      <div
+                        class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                        <div
+                          class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                          <h5 class="text-xl font-medium leading-normal text-gray-800" id="ModalLabel">NFT Detail</h5>
+                          <button type="button"
+                            class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                            data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body relative p-4">
+                          <p className="text-2xl font-bold text-black">{nftListDetail.name}</p>
+                          <p className="text-base text-black">Price: {nftListDetail.price} ETH</p>
+                          <p className="text-base text-black">Seller: {nftListDetail.seller}</p>
+                          <p className="text-base text-black">Owner: {nftListDetail.owner}</p>
+                        </div>
+                        <div
+                          class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+                          <button
+                            onClick={checkNft}
+                            disabled
+                            className="font-bold mx-10 bg-neutral-200 text-neutral-600 rounded p-4 shadow-md"
+                            >YOU ARE NOT ALLOWED TO SELL IT NOW
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
                   <div class="offcanvas offcanvas-bottom fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 left-0 right-0 border-none h-1/3 max-h-full" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
                     <div class="offcanvas-header flex items-center justify-between p-4 bg-current">
                       <h5 class="offcanvas-title mb-0 leading-normal font-semibold text-white" id="offcanvasBottomLabel">NFT Detail</h5>
